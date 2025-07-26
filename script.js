3356dcd18ec9eba52817cb42bbea7d6020558a3a
Kazak <strannik19742@mail.ru>
2025-07-26 16:39:42 +0300

    lesson03

---
+++ script.js
@@ -0,0 +1,34 @@
+const lang = "ru";
+let arr;
+
+if (lang == "ru") {
+  arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+} else if (lang == "en") {
+  arr = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
+}
+
+console.log(arr);
+
+var lang1 = "ru";
+let arr1;
+
+switch (true) {
+  case lang1 == "ru":
+    arr1 = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+    break;
+  case lang1 == "en":
+    arr1 = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
+}
+
+console.log(arr1);
+
+let langObj = "en";
+
+let obj = {
+  ru: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
+  en: ["mn", "ts", "wd", "th", "fr", "st", "sn"],
+};
+
+let arrObj = obj[langObj];
+
+console.log(arrObj);
