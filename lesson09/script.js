9a2f9ea967d17ac9b7480882f1fb961c6bff0785
Kazak <strannik19742@mail.ru>
2025-08-03 10:31:31 +0300

    lesson09

---
+++ lesson09/script.js
@@ -0,0 +1,141 @@
+"use strict";
+
+const monthArr = [
+  "января",
+  "февраля",
+  "марта",
+  "апреля",
+  "мая",
+  "июня",
+  "июля",
+  "августа",
+  "сентября",
+  "октября",
+  "ноября",
+  "декабря",
+];
+const weekArr = [
+  "понедельник",
+  "вторник",
+  "среда",
+  "четверг",
+  "пятница",
+  "суббота",
+  "воскресенье",
+];
+
+function getDateTime() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth();
+  let day = now.getDate();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+  let hourStr = "";
+
+  if (hour == 1 || hour == 21) {
+    hourStr = "час";
+  }
+  if (hour > 1 || hour < 20) {
+    hourStr = "часов";
+  }
+  if (hour > 21 || (hour > 1 && hour == 4)) {
+    hourStr = "часа";
+  }
+  console.log(
+    `Сегодня , ${day} ${monthArr[month]} ${year}, ${hour}  ${minute}   ${second}`
+  );
+  let time = `Сегодня , ${day} ${monthArr[month]} ${year}года, ${hour} ${hourStr}  ${minute}минут  ${second}секунд`;
+
+  document.getElementById("clock").innerText = time;
+}
+
+function getDateTime1() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+
+  if (hour.toString().length == 1) {
+    hour = "0" + hour;
+  }
+  if (minute.toString().length == 1) {
+    minute = "0" + minute;
+  }
+  if (second.toString().length == 1) {
+    second = "0" + second;
+  }
+  if (day.toString().length == 1) {
+    day = "0" + day;
+  }
+  if (month.toString().length == 1) {
+    month = "0" + month;
+  }
+  addendumNull(hour, minute, second, day);
+
+  let time1 = `${day} ${month} ${year}, ${hour}:${minute}:${second}`;
+
+  document.getElementById("clock1").innerHTML = time1;
+}
+
+function addendumNull(hour, minute, second, day) {
+  if (day.toString().length == 1) {
+    day = "0" + day;
+  }
+  if (hour.toString().length == 1) {
+    hour = "0" + hour;
+  }
+  if (minute.toString().length == 1) {
+    minute = "0" + minute;
+  }
+  if (second.toString().length == 1) {
+    second = "0" + second;
+  }
+  return;
+}
+
+function startClock() {
+  setInterval(function () {
+    getDateTime();
+  }, 1000);
+}
+
+function startClock1() {
+  setInterval(function () {
+    getDateTime1();
+  }, 1000);
+}
+startClock();
+startClock1();
+
+/* if (month.toString().length == 1) {
+    month = "0" + month;
+  }
+  if (day.toString().length == 1) {
+    day = "0" + day;
+  }
+  if (hour.toString().length == 1) {
+    hour = "0" + hour;
+  }
+  if (minute.toString().length == 1) {
+    minute = "0" + minute;
+  }
+  if (second.toString().length == 1) {
+    second = "0" + second;
+  }
+  var dateTime =
+    year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+  return dateTime;
+}
+
+// example usage: realtime clock
+setInterval(function () {
+  currentTime = getDateTime();
+  document.getElementById("digital-clock").innerHTML = currentTime;
+}, 1000);
+
+setInterval();*/
